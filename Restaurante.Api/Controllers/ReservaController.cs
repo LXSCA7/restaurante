7115128db04dd074127660bfd35d6044f1ad8da4
LXSCA <lucas.edr27@gmail.com>
2024-07-16 01:24:57 -0300

    feat: Criação de reserva.

--- Restaurante.Api/Controllers/ReservaController.cs
+++ Restaurante.Api/Controllers/ReservaController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Restaurante.Api.Context;
+using Restaurante.Api.Functions;
 using Restaurante.Api.Models;
 
 namespace Restaurante.Api.Controllers
@@ -20,7 +21,27 @@ namespace Restaurante.Api.Controllers
 
         [HttpPut("criar-reserva")]
         public IActionResult Create([FromBody] Reserva reserva)
-        {   
+        {
+            if (!Email.VerifyMail(reserva.EmailCliente))
+                return BadRequest($"O email \"{reserva.EmailCliente}\" é inválido");
+            
+            if (!Phone.VerifyPhone(reserva.TelefoneCliente, out string telefoneFormatado))
+                return BadRequest($"O telefone \"{reserva.TelefoneCliente}\" é inválido.");
+
+            reserva.TelefoneCliente = telefoneFormatado;
+
+            var verificaMesa = _context.Mesas.FirstOrDefault(m => m.Id == reserva.IdMesa);
+
+            if (verificaMesa == null)
+                return BadRequest("Mesa inválida.");
+            
+            var mesaReservada = _context.Reservas.First(m => m.IdMesa == reserva.IdMesa && m.DataHoraReserva == reserva.DataHoraReserva);
+            
+            if (mesaReservada != null)
+                return BadRequest("A mesa já está reservada.");
+
+            _context.Reservas.Add(reserva);
+            _context.SaveChanges();
             return Ok(reserva);
         }
     }
