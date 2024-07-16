24ea54b3331156903e2d592afa48a22dd9f903e2
LXSCA <lucas.edr27@gmail.com>
2024-07-16 01:25:10 -0300

    feat: migrações atualizadas.

--- Restaurante.Api/Migrations/20240716040010_MudancaTabela.Designer.cs
+++ Restaurante.Api/Migrations/20240716040010_MudancaTabela.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Restaurante.Api.Context;
 
@@ -11,9 +12,11 @@ using Restaurante.Api.Context;
 namespace Restaurante.Api.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240716040010_MudancaTabela")]
+    partial class MudancaTabela
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Restaurante.Api/Migrations/20240716040010_MudancaTabela.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurante.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MudancaTabela : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservas_Mesas_MesaId",
+                table: "Reservas");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reservas_Usuarios_UsuarioId",
+                table: "Reservas");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservas_MesaId",
+                table: "Reservas");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reservas_UsuarioId",
+                table: "Reservas");
+
+            migrationBuilder.DropColumn(
+                name: "HoraReserva",
+                table: "Reservas");
+
+            migrationBuilder.DropColumn(
+                name: "MesaId",
+                table: "Reservas");
+
+            migrationBuilder.DropColumn(
+                name: "UsuarioId",
+                table: "Reservas");
+
+            migrationBuilder.RenameColumn(
+                name: "IdUsuario",
+                table: "Reservas",
+                newName: "QuantidadeDePessoas");
+
+            migrationBuilder.RenameColumn(
+                name: "DataReserva",
+                table: "Reservas",
+                newName: "DataHoraReserva");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmailCliente",
+                table: "Reservas",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NomeCliente",
+                table: "Reservas",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TelefoneCliente",
+                table: "Reservas",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailCliente",
+                table: "Reservas");
+
+            migrationBuilder.DropColumn(
+                name: "NomeCliente",
+                table: "Reservas");
+
+            migrationBuilder.DropColumn(
+                name: "TelefoneCliente",
+                table: "Reservas");
+
+            migrationBuilder.RenameColumn(
+                name: "QuantidadeDePessoas",
+                table: "Reservas",
+                newName: "IdUsuario");
+
+            migrationBuilder.RenameColumn(
+                name: "DataHoraReserva",
+                table: "Reservas",
+                newName: "DataReserva");
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "HoraReserva",
+                table: "Reservas",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<int>(
+                name: "MesaId",
+                table: "Reservas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UsuarioId",
+                table: "Reservas",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Telefone = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservas_MesaId",
+                table: "Reservas",
+                column: "MesaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservas_UsuarioId",
+                table: "Reservas",
+                column: "UsuarioId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservas_Mesas_MesaId",
+                table: "Reservas",
+                column: "MesaId",
+                principalTable: "Mesas",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reservas_Usuarios_UsuarioId",
+                table: "Reservas",
+                column: "UsuarioId",
+                principalTable: "Usuarios",
+                principalColumn: "Id");
+        }
+    }
+}
